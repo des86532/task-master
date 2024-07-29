97774b90cd83a66504e8aa75b8c2a0f7b9d60b2a
Neil Yang <des75421@gmail.com>
2024-07-30 01:44:17 +0800

    chore: turn off eslint no-unused-vars & git ignore .nx/workspace-data & project path alias setting

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -16,8 +16,8 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/board',
-        permanent: true, // 301 永久重定向，如果你只是暂时的重定向，可以设置为 false
+        destination: '/list',
+        permanent: false, // 301 永久重定向，如果你只是暂时的重定向，可以设置为 false
       },
     ];
   },
